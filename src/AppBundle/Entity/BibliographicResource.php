94155d929f99044226faf3e9058bb83589621ecc
stuckime <melanie.stucki@outlook.com>
2016-08-16 12:40:30 +0200

    adds new entitys BibliographicResource.php and Item.php

---
+++ src/AppBundle/Entity/BibliographicResource.php
@@ -0,0 +1,160 @@
+<?php
+namespace AppBundle\Entity;
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+/**
+ * A BiobligraphicResource.
+ *
+ * @see http://purl.org/dc/terms/BibliographicResource Documentation on purl.org
+ *
+ * @ApiResource(
+ *     iri="http://purl.org/dc/terms/BibliographicResource",
+ *     itemOperations={
+ *          "get":{"method":"GET"}
+ *     },
+ *     collectionOperations={
+ *          "get":{"method":"GET"}
+ *     }
+ * )
+ */
+class BibliographicResource
+{
+    /**
+     * @var string
+     *
+     * @ApiProperty(identifier=true)
+     */
+    private $id;
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/title")
+     */
+    private $title;
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="https://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+     */
+    private $type;
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/language")
+     */
+    private $language;
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://purl.org/dc/elements/1.1/format")
+     */
+    private $format;
+    /**
+     * @var array
+     *
+     * @ApiProperty(readable=false)
+     */
+    private $source;
+    /**
+     * BiobligraphicResource constructor.
+     *
+     * @param array $response
+     */
+    public function __construct(array $response)
+    {
+        $this->title = $response['_source']['dct:title'] ?? null;
+        $this->type = $response['_source']['rdf:type'] ?? null;
+        $this->language = $response['_source']['dct:language'] ?? null;
+        $this->format = $response['_source']['dc:format'] ?? null;
+        $this->id = $response['_id'];
+        $this->source = $response['_source'];
+    }
+    /**
+     * @return string
+     */
+    public function getTitle() : string
+    {
+        return $this->title ?? '';
+    }
+    /**
+     * @param string $title
+     *
+     * @return string
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+    }
+    /**
+     * @return string
+     */
+    public function getId() : string
+    {
+        return $this->id ?? '';
+    }
+    /**
+     * @param string $id
+     *
+     * @return string
+     */
+    public function setId(string $id)
+    {
+        $this->id = $id;
+    }
+    /**
+     * @return array
+     */
+    public function getSource() : array
+    {
+        return $this->source;
+    }
+    /**
+     * @return string
+     */
+    public function getType() : string
+    {
+        return $this->type ?? '';
+    }
+    /**
+     * @param string $type
+     *
+     * @return string
+     */
+    public function setType(string $type)
+    {
+        $this->type = $type;
+    }
+    /**
+     * @return string
+     */
+    public function getLanguage() : string
+    {
+        return $this->language ?? '';
+    }
+    /**
+     * @param string $language
+     *
+     * @return string
+     */
+    public function setLanguage(string $language)
+    {
+        $this->language = $language;
+    }
+    /**
+     * @return string
+     */
+    public function getFormat() : string
+    {
+        return $this->format ?? '';
+    }
+    /**
+     * @param string $format
+     *
+     * @return string
+     */
+    public function setFormat(string $format)
+    {
+        $this->format = $format;
+    }
+}
---
+++ src/AppBundle/Entity/Item.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+
+/**
+ * An Item.
+ *
+ * @see http://bibframe.org/vocab/HeldItem
+ *
+ * @ApiResource(
+ *     iri="http://bibframe.org/vocab/HeldItem",
+ *     itemOperations={
+ *          "get":{"method":"GET"}
+ *     },
+ *     collectionOperations={
+ *          "get":{"method":"GET"}
+ *     }
+ * )
+ */
+class Item
+{
+    /**
+     * @var string
+     *
+     * @ApiProperty(identifier=true)
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="https://www.w3.org/1999/02/22-rdf-syntax-ns#type")
+     */
+    private $type;
+
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="?")
+     */
+    private $context;
+
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://bibframe.org/vocab/holdingFor")
+     */
+    private $holdingFor;
+
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://xmlns.com/foaf/0.1/page")
+     */
+    private $page;
+
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://purl.org/ontology/bibo/owner")
+     */
+    private $owner;
+
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://purl.org/ontology/bibo/locator")
+     */
+    private $locator;
+
+    /**
+     * @var string
+     *
+     * @ApiProperty(iri="http://bibframe.org/vocab/subLocation")
+     */
+    private $subLocation;
+
+    /**
+     * BiobligraphicResource constructor.
+     *
+     * @param array $response
+     */
+    public function __construct(array $response)
+    {
+        $this->id = $response['_id'];
+        $this->context = $response['_source'];
+        $this->type = $response['_source'];
+        $this->holdingFor = $response['_source']['bf:holdingFor'] ?? null;
+        $this->subLocation = $response['_source']['bf:subLocation'] ?? null;
+        $this->locator = $response['_source']['bibo:locator'] ?? null;
+        $this->owner = $response['_source']['bibo:owner'] ?? null;
+        $this->page  = $response['_source']['foaf:page'] ?? null;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId() : string
+    {
+        return $this->id ?? '';
+    }
+    /**
+     * @return string
+     */
+    public function getContext() : string
+    {
+        return $this->context ?? '';
+    }
+    /**
+     * @return string
+     */
+    public function getType() : string
+    {
+        return $this->type ?? '';
+    }
+    /**
+     * @return string
+     */
+    public function getHoldingFor() : string
+    {
+        return $this->holdingFor ?? '';
+    }
+    /**
+     * @return string
+     */
+    public function getSubLocation() : string
+    {
+        return $this->subLocation ?? '';
+    }
+    /**
+     * @return string
+     */
+    public function getLocator() : string
+    {
+        return $this->locator ?? '';
+    }
+    /**
+     * @return string
+     */
+    public function getOwner() : string
+    {
+        return $this->owner ?? '';
+    }
+    /**
+     * @return string
+     */
+    public function getPage() : string
+    {
+        return $this->page ?? '';
+    }
+}
