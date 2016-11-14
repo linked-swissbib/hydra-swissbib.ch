23882ed087ee9a98cf64c88143859bc5e896b1d7
stuckime <meli.stucki93@hotmail.com>
2016-11-14 10:40:24 +0100

    Adds missing attributes in BibliographicResource.php

--- src/LinkedSwissbibBundle/Entity/BibliographicResource.php
+++ src/LinkedSwissbibBundle/Entity/BibliographicResource.php
@@ -36,7 +36,7 @@ class BibliographicResource
      * @ApiProperty(identifier=true)
      */
     private $id;
-    
+
     /**
      * @var string|array Title of a bibliographic resource
      *
@@ -47,7 +47,7 @@ class BibliographicResource
      * })
      */
     private $title;
-    
+
     /**
      * @var string|array
      *
@@ -58,7 +58,7 @@ class BibliographicResource
      * })
      */
     private $type;
-    
+
     /**
      * @var string|array Written language of the bibliographic resource
      *
@@ -69,7 +69,18 @@ class BibliographicResource
      * })
      */
     private $language;
-    
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://bibframe.org/vocab/instanceOf",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $instanceOf;
+
     /**
      * @var string|array
      *
@@ -82,6 +93,244 @@ class BibliographicResource
     private $format;
 
     /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/ontology/bibo/edition",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $edition;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/ontology/bibo/isbn10",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $isbn10;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/ontology/bibo/isbn13",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $isbn13;
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/ontology/bibo/issn",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $issn;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://dbpedia.org/ontology/originalLanguage",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $originalLanguage;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/alternative",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $alternative;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/bibliographicCitation",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $bibliographicCitation;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/contributor",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $contributor;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/hasPart",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $hasPart;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/isPartOf",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $isPartOf;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/issued",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $issued;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://purl.org/dc/terms/subject",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $subject;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60049",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $P60049;
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60050",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $P60050;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60051",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $P60051;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60163",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $P60163;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60333",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $P60333;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60339",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $P60339;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60470",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $P60470;
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://rdaregistry.info/Elements/u/P60489",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="http://www.w3.org/2001/XMLSchema#string"
+     *     }
+     * })
+     */
+    private $P60489;
+
+
+    /**
+     * @var string|array
+     *
+     * @ApiProperty(iri="http://www.w3.org/2000/01/rdf-schema#/idDefinedBy",writable=false, attributes={
+     *     "jsonld_context"={
+     *         "@type"="@id"
+     *     }
+     * })
+     */
+    private $isDefinedBy;
+
+    /**
+     * @return array|string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
      * @param array|string $id
      */
     public function setId($id)
@@ -90,11 +339,11 @@ class BibliographicResource
     }
 
     /**
-     * @return string|array
+     * @return array|string
      */
-    public function getId()
+    public function getTitle()
     {
-        return $this->id;
+        return $this->title;
     }
 
     /**
@@ -106,11 +355,11 @@ class BibliographicResource
     }
 
     /**
-     * @return string|array
+     * @return array|string
      */
-    public function getTitle()
+    public function getType()
     {
-        return $this->title;
+        return $this->type;
     }
 
     /**
@@ -122,11 +371,11 @@ class BibliographicResource
     }
 
     /**
-     * @return string |array
-    */
-    public function getType()
+     * @return array|string
+     */
+    public function getLanguage()
     {
-        return $this->type;
+        return $this->language;
     }
 
     /**
@@ -136,13 +385,29 @@ class BibliographicResource
     {
         $this->language = $language;
     }
-    
+
     /**
-     * @return string|array
+     * @return array|string
      */
-    public function getLanguage()
+    public function getInstanceOf()
     {
-        return $this->language;
+        return $this->instanceOf;
+    }
+
+    /**
+     * @param array|string $instanceOf
+     */
+    public function setInstanceOf($instanceOf)
+    {
+        $this->instanceOf = $instanceOf;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getFormat()
+    {
+        return $this->format;
     }
 
     /**
@@ -154,10 +419,338 @@ class BibliographicResource
     }
 
     /**
-     * @return string|array
+     * @return array|string
      */
-    public function getFormat()
+    public function getEdition()
     {
-        return $this->format;
+        return $this->edition;
+    }
+
+    /**
+     * @param array|string $edition
+     */
+    public function setEdition($edition)
+    {
+        $this->edition = $edition;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getIsbn10()
+    {
+        return $this->isbn10;
+    }
+
+    /**
+     * @param array|string $isbn10
+     */
+    public function setIsbn10($isbn10)
+    {
+        $this->isbn10 = $isbn10;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getIsbn13()
+    {
+        return $this->isbn13;
+    }
+
+    /**
+     * @param array|string $isbn13
+     */
+    public function setIsbn13($isbn13)
+    {
+        $this->isbn13 = $isbn13;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getIssn()
+    {
+        return $this->issn;
+    }
+
+    /**
+     * @param array|string $issn
+     */
+    public function setIssn($issn)
+    {
+        $this->issn = $issn;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getOriginalLanguage()
+    {
+        return $this->originalLanguage;
+    }
+
+    /**
+     * @param array|string $originalLanguage
+     */
+    public function setOriginalLanguage($originalLanguage)
+    {
+        $this->originalLanguage = $originalLanguage;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getAlternative()
+    {
+        return $this->alternative;
+    }
+
+    /**
+     * @param array|string $alternative
+     */
+    public function setAlternative($alternative)
+    {
+        $this->alternative = $alternative;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getBibliographicCitation()
+    {
+        return $this->bibliographicCitation;
+    }
+
+    /**
+     * @param array|string $bibliographicCitation
+     */
+    public function setBibliographicCitation($bibliographicCitation)
+    {
+        $this->bibliographicCitation = $bibliographicCitation;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getContributor()
+    {
+        return $this->contributor;
+    }
+
+    /**
+     * @param array|string $contributor
+     */
+    public function setContributor($contributor)
+    {
+        $this->contributor = $contributor;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getHasPart()
+    {
+        return $this->hasPart;
+    }
+
+    /**
+     * @param array|string $hasPart
+     */
+    public function setHasPart($hasPart)
+    {
+        $this->hasPart = $hasPart;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getIsPartOf()
+    {
+        return $this->isPartOf;
+    }
+
+    /**
+     * @param array|string $isPartOf
+     */
+    public function setIsPartOf($isPartOf)
+    {
+        $this->isPartOf = $isPartOf;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getIssued()
+    {
+        return $this->issued;
+    }
+
+    /**
+     * @param array|string $issued
+     */
+    public function setIssued($issued)
+    {
+        $this->issued = $issued;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getSubject()
+    {
+        return $this->subject;
+    }
+
+    /**
+     * @param array|string $subject
+     */
+    public function setSubject($subject)
+    {
+        $this->subject = $subject;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60049()
+    {
+        return $this->P60049;
+    }
+
+    /**
+     * @param array|string $P60049
+     */
+    public function setP60049($P60049)
+    {
+        $this->P60049 = $P60049;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60050()
+    {
+        return $this->P60050;
+    }
+
+    /**
+     * @param array|string $P60050
+     */
+    public function setP60050($P60050)
+    {
+        $this->P60050 = $P60050;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60051()
+    {
+        return $this->P60051;
+    }
+
+    /**
+     * @param array|string $P60051
+     */
+    public function setP60051($P60051)
+    {
+        $this->P60051 = $P60051;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60163()
+    {
+        return $this->P60163;
+    }
+
+    /**
+     * @param array|string $P60163
+     */
+    public function setP60163($P60163)
+    {
+        $this->P60163 = $P60163;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60333()
+    {
+        return $this->P60333;
+    }
+
+    /**
+     * @param array|string $P60333
+     */
+    public function setP60333($P60333)
+    {
+        $this->P60333 = $P60333;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60339()
+    {
+        return $this->P60339;
+    }
+
+    /**
+     * @param array|string $P60339
+     */
+    public function setP60339($P60339)
+    {
+        $this->P60339 = $P60339;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60470()
+    {
+        return $this->P60470;
+    }
+
+    /**
+     * @param array|string $P60470
+     */
+    public function setP60470($P60470)
+    {
+        $this->P60470 = $P60470;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getP60489()
+    {
+        return $this->P60489;
+    }
+
+    /**
+     * @param array|string $P60489
+     */
+    public function setP60489($P60489)
+    {
+        $this->P60489 = $P60489;
+    }
+
+    /**
+     * @return array|string
+     */
+    public function getIsDefinedBy()
+    {
+        return $this->isDefinedBy;
+    }
+
+    /**
+     * @param array|string $isDefinedBy
+     */
+    public function setIsDefinedBy($isDefinedBy)
+    {
+        $this->isDefinedBy = $isDefinedBy;
     }
 }
