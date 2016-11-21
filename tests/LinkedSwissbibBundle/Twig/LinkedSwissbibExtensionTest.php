fc7e11267e75a7b87b8a984661ef553bc3849fa8
Markus Mächler <markus.maechler@bithost.ch>
2016-11-21 10:00:38 +0100

    Adds twig extension test

---
+++ tests/LinkedSwissbibBundle/Twig/LinkedSwissbibExtensionTest.php
@@ -0,0 +1,78 @@
+<?php
+namespace Tests\LinkedSwissbibBundle\Twig;
+
+use \LinkedSwissbibBundle\Twig\LinkedSwissbibExtension;
+
+/**
+ * LinkedSwissbibExtension
+ *
+ * @author   Melanie Stucki <melanie.stucki@students.fhnw.ch>, Markus Mächler <markus.maechler@students.fhnw.ch>
+ * @license  http://opensource.org/licenses/gpl-2.0.php
+ * @link     http://linked.swissbib.ch
+ */
+class LinkedSwissbibExtensionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var LinkedSwissbibExtension
+     */
+    protected $extension;
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->extension = new LinkedSwissbibExtension();
+    }
+
+    /**
+     * @return void
+     */
+    public function testString()
+    {
+        $this->assertEquals('string', $this->extension->typeFunction('string test'));
+    }
+
+    /**
+     * @return void
+     */
+    public function testNumber()
+    {
+        $this->assertEquals('number', $this->extension->typeFunction(1));
+        $this->assertEquals('number', $this->extension->typeFunction(1.1));
+        $this->assertEquals('number', $this->extension->typeFunction(0));
+    }
+
+    /**
+     * @return void
+     */
+    public function testBool()
+    {
+        $this->assertEquals('bool', $this->extension->typeFunction(true));
+        $this->assertEquals('bool', $this->extension->typeFunction(false));
+    }
+
+    /**
+     * @return void
+     */
+    public function testObject()
+    {
+        $this->assertEquals('object', $this->extension->typeFunction(new \stdClass()));
+    }
+
+    /**
+     * @return void
+     */
+    public function testArray()
+    {
+        $this->assertEquals('array', $this->extension->typeFunction([]));
+        $this->assertEquals('array', $this->extension->typeFunction([1, 2, 3]));
+    }
+
+    /**
+     * @return void
+     */
+    public function testUndefined()
+    {
+        $this->assertEquals('undefined', $this->extension->typeFunction(null));
+    }
+}
